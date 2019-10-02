019a53a8f286a503b9a487bafb5f4d25d81583a4
John Cadena <cadena.john.85@gmail.com>
2019-10-02 13:55:44 -0500

    create initial files for ContactApplication project

---
+++ src/Contact.java
@@ -0,0 +1,11 @@
+public class Contact {
+    private String name;
+    private String number;
+
+    public Contact(String name, String number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    // Add getters and setters later if needed
+}
---
+++ src/ContactApplication.java
@@ -0,0 +1,17 @@
+public class ContactApplication {
+
+    public static void main (String[] args) {
+
+    }
+
+    // displayMenu // Print menu screen, return an int representing the user's option
+    // processUserChoice ||  takes in an int rep. user option, processes with a switch statement
+        // 1. View contacts.
+        // 2. Add a new contact.
+        // 3. Search a contact by name.
+        // 4. Delete an existing contact.
+            // 5. Exit. // set looping variable equal to false
+    // Write to text file upon exiting application
+
+
+}
